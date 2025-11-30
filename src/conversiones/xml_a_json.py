e3ac36067ccb21a3cfffe64ffad5a63a68710087
aaron050223 <agallardoc@iesrafaelalberti.es>
2025-11-30 17:17:25 +0100

    Edito la conversión de XML a JSON

--- src/conversiones/xml_a_json.py
+++ src/conversiones/xml_a_json.py
@@ -7,8 +7,8 @@ try:
 
     with open("src/conversiones/creados/conversion.json", "w", encoding="utf-8") as conversion:
         json.dump(diccionario_json, conversion, indent=4)
-        
+
 except FileNotFoundError:
     print("ERROR: No se encuentra el JSON")
 except Exception as error:
-    print(f"ERROR: {error}")
+    print(f"ERROR: {error}") 
